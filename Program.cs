0a9ec76c2f7ecf112c9fd2aae213fd14dd8ce565
jullian_barber <jullian.barber@gmail.com>
2015-03-04 23:35:33 +0000

    Creating MBS calculaion

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.ReadKey();
+
+            String myname = "";            
+            Console.WriteLine("---->" + myname);
+            
+            myname = "jullianbarber";
+            Console.WriteLine("The value of yname is:" + myname);
+
+            int a = 54;
+            int b = 32;
+            double dbl = 17;
+
+            Console.WriteLine("a:" +a + "b:" + b + "dbl:" + dbl);
+            Console.WriteLine( dbl/2);
+            Console.WriteLine(a * a);
+            Console.WriteLine(b * b);
+
+            double a2 = a * a;
+            double b2 = b * b;
+            double dblhalf = dbl / 2;
+
+            Console.WriteLine(a2 + b2);
+
+            Console.WriteLine(Math.Sqrt(a2 + b2) + dblhalf);
+
+
+
+           Console.ReadKey();
         }
     }
 
